95ab41bc6d1ba3c208dab18520863e21f2f1aa6e
arix00 <15333224+arix00@users.noreply.github.com>
2024-09-22 01:07:23 -0700

    Update genconfig_nss.sh
    Signed-off-by: arix00 <15333224+arix00@users.noreply.github.com>

--- genconfig_nss.sh
+++ genconfig_nss.sh
@@ -5,5 +5,6 @@ cp nss-setup/config-nss.seed .config
 echo CONFIG_ATH11K_THERMAL=y >> .config
 echo CONFIG_KMOD_ALL=y >> .config
 echo CONFIG_ATH10K=n >> .config
+echo "# CONFIG_NF_CONNTRACK_DSCPREMARK_EXT is not set" >> target/linux/generic/config-6.6
 
 make defconfig
