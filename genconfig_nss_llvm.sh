032ba679a580e921796bc1f9a8bf1385fb7eecbd
arix00 <15333224+arix00@users.noreply.github.com>
2024-09-22 10:05:26 -0700

    Update genconfig_nss_llvm.sh
    Signed-off-by: arix00 <15333224+arix00@users.noreply.github.com>

--- genconfig_nss_llvm.sh
+++ genconfig_nss_llvm.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
-cat nss-setup/config-nss.seed | grep -v CONFIG_TARGET_OPTIMIZATION | grep -v CONFIG_CCACHE > .config
+wget  https://downloads.openwrt.org/snapshots/targets/qualcommax/ipq807x/config.buildinfo -O config.buildinfo
+cat config.buildinfo | grep -v CONFIG_TARGET_ | grep -v CONFIG_IB | grep -v ONFIG_PACKAGE > .config
+
+cat nss-setup/config-nss.seed | grep -v CONFIG_TARGET_OPTIMIZATION | grep -v CONFIG_CCACHE >> .config
 echo CONFIG_ATH11K_THERMAL=y >> .config
 echo CONFIG_ALL_KMODS=y >> .config
-echo CONFIG_ATH10K=n >> .config
 
-wget  https://downloads.openwrt.org/snapshots/targets/qualcommax/ipq807x/config.buildinfo -O config.buildinfo
-cat config.buildinfo | grep -v CONFIG_TARGET_ | grep -v CONFIG_IB | grep -v ONFIG_PACKAGE >> .config
-
-echo "# CONFIG_NF_CONNTRACK_DSCPREMARK_EXT is not set" >> target/linux/generic/config-6.6
-
+grep -q CONFIG_NF_CONNTRACK_DSCPREMARK_EXT target/linux/generic/config-6.6 || echo "# CONFIG_NF_CONNTRACK_DSCPREMARK_EXT is not set" >> target/linux/generic/config-6.6
 make defconfig
 
-#skip xdp
-cat .config | grep -v "CONFIG_PACKAGE.*xdp" > .config.tmp
+echo "CONFIG_PACKAGE_kmod-ath10k=n
+CONFIG_ATH10K_LEDS=n
+CONFIG_ATH10K_THERMAL=n
+CONFIG_PACKAGE_kmod-ath10k-ct=n
+CONFIG_ATH10K-CT_LEDS=n
+CONFIG_PACKAGE_kmod-ath10k-ct-smallbuffers=n
+CONFIG_PACKAGE_kmod-ath10k-smallbuffers=n"  >> .config
+
+cat .config | grep -v CONFIG_ALL_KMODS  > .config.tmp
 cp .config.tmp .config
+
+make defconfig
