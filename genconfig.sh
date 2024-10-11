44f4ebc5375aa2bfdd93d9009b16b9c450614bae
Kyo <kyogithub@users.noreply.github.com>
2024-10-11 17:36:45 -0500

    Update genconfig.sh
    Signed-off-by: Kyo <kyogithub@users.noreply.github.com>

--- genconfig.sh
+++ genconfig.sh
@@ -10,7 +10,7 @@ echo CONFIG_TARGET_DEVICE_PACKAGES_qualcommax_ipq807x_DEVICE_linksys_mx4300=\"\"
 echo CONFIG_PACKAGE_luci=y >> .config
 
 #add extras
-source kyo_customs/config.cnf
+cat kyo_customs/config.cnf >> .config
 
 make defconfig
 
