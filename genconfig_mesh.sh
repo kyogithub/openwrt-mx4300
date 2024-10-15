72b67b3db9487ed946db8d18ac4d3d9c682ec81b
Kyo <kyogithub@users.noreply.github.com>
2024-10-15 00:13:40 -0500

    Update genconfig_mesh.sh
    Signed-off-by: Kyo <kyogithub@users.noreply.github.com>

--- genconfig_mesh.sh
+++ genconfig_mesh.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-echo "- ${CURDIR} "
-
 wget -qO- https://downloads.openwrt.org/snapshots/targets/qualcommax/ipq807x/config.buildinfo | grep -v CONFIG_TARGET_DEVICE_ | grep -v CONFIG_TARGET_ALL_PROFILES | grep -v CONFIG_TARGET_MULTI_PROFILE > .config
 echo CONFIG_TARGET_ALL_PROFILES=n >> .config
 echo CONFIG_TARGET_MULTI_PROFILE=n >> .config
