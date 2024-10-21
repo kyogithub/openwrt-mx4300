da6339929f6b83c6699f86b7359c21cea2e07819
Kyo <kyogithub@users.noreply.github.com>
2024-10-21 16:29:42 -0500

    Update genconfig_repeater.sh
    Signed-off-by: Kyo <kyogithub@users.noreply.github.com>

--- kyo_customs/genconfig_repeater.sh
+++ kyo_customs/genconfig_repeater.sh
@@ -12,7 +12,7 @@ echo CONFIG_PACKAGE_luci=y >> .config
 #add extras
 cat kyo_customs/repeater.cnf >> .config
 
-make defconfig
+make defconfig download clean
 
 #add libpam
 #echo CONFIG_PACKAGE_libpam=y >> .config
