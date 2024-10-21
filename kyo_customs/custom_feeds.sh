132ae20fbb43beb2932851046e345816530ed75e
Kyo <kyogithub@users.noreply.github.com>
2024-10-21 16:53:45 -0500

    Update custom_feeds.sh
    Signed-off-by: Kyo <kyogithub@users.noreply.github.com>

--- kyo_customs/custom_feeds.sh
+++ kyo_customs/custom_feeds.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git clone -b master https://github.com/jerrykuku/luci-theme-argon.git package/downloads/luci-theme-argon
 
-sed -i '$a src-git luciargon https://github.com/jerrykuku/luci-theme-argon.git;master' feeds.conf.default
+#sed -i '$a src-git luciargon https://github.com/jerrykuku/luci-theme-argon.git;master' feeds.conf.default
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
