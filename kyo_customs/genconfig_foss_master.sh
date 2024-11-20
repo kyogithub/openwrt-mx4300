975c565333f75ca70b5de811841dce62d0a255ae
Kyo <kyogithub@users.noreply.github.com>
2024-11-19 22:34:13 -0500

    Update genconfig_foss_master.sh
    Signed-off-by: Kyo <kyogithub@users.noreply.github.com>

--- kyo_customs/genconfig_foss_master.sh
+++ kyo_customs/genconfig_foss_master.sh
@@ -20,7 +20,7 @@ echo CONFIG_TARGET_DEVICE_PACKAGES_qualcommax_ipq807x_DEVICE_linksys_mx4300=\"\"
 #add luci
 echo CONFIG_PACKAGE_luci=y >> .config
 #add extras
-cat kyo_customs/master.cnf >> .config
+cat kyo_customs/config_master >> .config
 make defconfig
 
 #add libpam
