7e2eae9fc13580f67e5dd3052151274e2feea76a
Kyo <kyogithub@users.noreply.github.com>
2024-11-19 22:09:23 -0500

    Create genconfig_foss_node.sh
    Signed-off-by: Kyo <kyogithub@users.noreply.github.com>

---
+++ kyo_customs/genconfig_foss_node.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+#run locally
+[ -z $GITHUB_ENV ] && GITHUB_ENV=/tmp/GITHUB_ENV.txt
+
+if [ ! -z "$1" ] && [ "$1" != "snapshot" ]; then
+  buildinfo="https://downloads.openwrt.org/releases/$1/targets/qualcommax/ipq807x/config.buildinfo"
+  echo "ver=$1" >> $GITHUB_ENV
+else
+  buildinfo="https://downloads.openwrt.org/snapshots/targets/qualcommax/ipq807x/config.buildinfo"
+  echo "ver=snapshot" >> $GITHUB_ENV
+fi
+
+wget -qO- $buildinfo | grep -v CONFIG_TARGET_DEVICE_ | grep -v CONFIG_TARGET_ALL_PROFILES | grep -v CONFIG_TARGET_MULTI_PROFILE > .config
+echo CONFIG_TARGET_ALL_PROFILES=n >> .config
+echo CONFIG_TARGET_MULTI_PROFILE=n >> .config
+echo CONFIG_TARGET_qualcommax_ipq807x_DEVICE_linksys_mx4300=y >> .config
+echo CONFIG_TARGET_DEVICE_qualcommax_ipq807x_DEVICE_linksys_mx4300=y >> .config
+echo CONFIG_TARGET_DEVICE_PACKAGES_qualcommax_ipq807x_DEVICE_linksys_mx4300=\"\" >> .config
+#add luci
+echo CONFIG_PACKAGE_luci=y >> .config
+#add extras
+cat kyo_customs/repeater.cnf >> .config
+
+make defconfig
+
+#add libpam
+#echo CONFIG_PACKAGE_libpam=y >> .config
+
+#skip xdp compile
+#cat .config | grep -v "CONFIG_PACKAGE.*xdp" > .config.tmp
+#cp .config.tmp .config
