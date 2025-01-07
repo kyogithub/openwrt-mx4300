b61ad3d01882ecf720049166083b63d4550c2056
Kyo <kyogithub@users.noreply.github.com>
2025-01-06 22:56:41 -0500

    Update init_build.sh
    Signed-off-by: Kyo <kyogithub@users.noreply.github.com>

--- script/init_build.sh
+++ script/init_build.sh
@@ -13,17 +13,29 @@ tag="" #commit hash
 if [ $type = "foss" ]; then    
     #official MX4300 PR from testuser7
     #https://github.com/openwrt/openwrt/pull/16070
-    PATCH="https://github.com/openwrt/openwrt/pull/16070.diff"
+    #only necessary in 24.10 atm.
+    #PATCH="https://github.com/openwrt/openwrt/pull/16070.diff"
+    case $ver in
+        "snapshot")
+            PATCH="  "
+            ;;
+        "24.10"*)
+            PATCH="https://github.com/openwrt/openwrt/pull/16070.diff"
+            ;;
+    esac
 elif [ $type = "nss" ]; then
     #qosmio NSS patch
     #https://github.com/qosmio/openwrt-ipq
+    #for snapshot, apply "main-nss" 
+    #for 24.10, apply "24.10-nss-mx4300" as PR16070 failed on ipq8174-mx4200.dtsi
     case $ver in
         "snapshot")   
-            PATCH="https://github.com/openwrt/openwrt/compare/main...qosmio:openwrt-ipq:main-nss-mx4300.diff"
-            NSSBRANCH="main-nss-mx4300"
+            PATCH="https://github.com/openwrt/openwrt/compare/main...qosmio:openwrt-ipq:main-nss.diff"
+            NSSBRANCH="main-nss"
             ;;
         "24.10"*)
             PATCH="https://github.com/openwrt/openwrt/compare/openwrt-24.10...qosmio:openwrt-ipq:24.10-nss-mx4300.diff"
+            #PATCH="https://github.com/openwrt/openwrt/compare/openwrt-24.10...qosmio:openwrt-ipq:24.10-nss.diff https://github.com/openwrt/openwrt/pull/16070.diff"
             NSSBRANCH="24.10-nss-mx4300"
             ;;
     esac
@@ -49,8 +61,7 @@ if [ $sync = "y" ]; then
 fi
 
 echo $PATCH
-curl -L $PATCH -o mx4300.diff
-patch -p1 < mx4300.diff
+for p in $PATCH; do curl -L $p | patch -p1; done
 
 #1. support both 24.10-snapshot and (tagged) release
 #2. Handle package/firmware/ipq-wifi/Makefile in *possible* patch failure.
@@ -73,4 +84,3 @@ fi
 rej=$(find . -name "*.rej"  | wc -l)
 if [ ! $rej = "0" ]; then
     exit 1
-fi
