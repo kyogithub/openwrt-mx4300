4b5e32712bd6205eea406796368e207786cc884e
Kyo <kyogithub@users.noreply.github.com>
2025-04-02 17:31:27 -0500

    Update genconfig_nss.sh
    Signed-off-by: Kyo <kyogithub@users.noreply.github.com>

--- script/genconfig_nss.sh
+++ script/genconfig_nss.sh
@@ -14,13 +14,19 @@ else
   mainpath="https://downloads.openwrt.org/releases/$ver/targets/qualcommax/ipq807x"
 fi
 
-
+count=0
 if [ "${buildkmod}" != "n" ]; then
-  jsonpath=$(wget ${mainpath}/sha256sums -O - | grep index.json -m 1 | cut -d '*' -f 2 | xargs)
+  jsonpath=$(wget ${mainpath}/sha256sums -O - | grep kmods.*index.json | tail -n 1 | cut -d '*' -f 2 | xargs)
   kmods=$(wget ${mainpath}/${jsonpath} -O - | jq '.packages' | grep \"kmod | cut -d '"' -f 2)
-  #kmods=$(echo "$kmods" | grep -v ath | grep -v kmod-bonding | grep -v vxlan | grep -v kmod-nat46)
-  kmods=$(echo "$kmods" | grep -v ath)
-  echo extra kmods: $(echo $kmods | wc -w)
+  #fix dependency for 24.10 branch
+  if [ "$ver" != "${ver#24.10}" ]; then
+    kmods=$(echo "$kmods" | grep -v ath | grep -v kmod-bonding | grep -v vxlan | grep -v kmod-nat46)
+  else
+    kmods=$(echo "$kmods" | grep -v ath)
+  fi
+  count=$(echo $kmods | wc -w)
+  echo extra kmods: $count
+  if [ $count -lt 500 ]; then exit 1; fi
 fi
 
 cat nss-setup/config-nss.seed |  grep -v CONFIG_PACKAGE_luci >> .config
@@ -32,6 +38,13 @@ CONFIG_FEED_nss_packages=n
 make defconfig
 
 for k in $kmods; do grep -q $k=y .config || echo CONFIG_PACKAGE_$k=m >> .config; done
+#nss patch has some dependency issue on 24.10 + kmods
+#to fix:
+#kmod-vxlan and kmod-nat46 must be compiled as y to keep kmod-qca-nss-ecm as y
+#must skip kmod-bonding or compile fail
+#no problem in snapshot as they all set to y
+[ "$ver" != "${ver#24.10}" ] && for i in vxlan nat46; do echo "CONFIG_PACKAGE_kmod-$i=y" >> .config; done
+
 make defconfig
 
 cat .config | grep kmod-qca | grep -v "not set"
