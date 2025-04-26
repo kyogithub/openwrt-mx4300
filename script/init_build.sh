78a4ad6a0bc79a9cad1b29ba51315729a3883f90
Kyo <kyogithub@users.noreply.github.com>
2025-04-25 22:01:09 -0500

    Update init_build.sh
    Signed-off-by: Kyo <kyogithub@users.noreply.github.com>

--- script/init_build.sh
+++ script/init_build.sh
@@ -27,16 +27,16 @@ elif [ $type = "nss" ]; then
     #qosmio NSS patch
     #https://github.com/qosmio/openwrt-ipq
     #for snapshot, apply "main-nss" 
-    #for 24.10, apply "24.10-nss-mx4300" as PR16070 failed on ipq8174-mx4200.dtsi
+    #for 24.10, apply "24.10-nss" as MX4300 support is added in 24.10
     case $ver in
         "snapshot")   
             PATCH="https://github.com/openwrt/openwrt/compare/main...qosmio:openwrt-ipq:main-nss.diff"
             NSSBRANCH="main-nss"
             ;;
         "24.10"*)
-            PATCH="https://github.com/openwrt/openwrt/compare/openwrt-24.10...qosmio:openwrt-ipq:24.10-nss-mx4300.diff"
+            PATCH="https://github.com/openwrt/openwrt/compare/openwrt-24.10...qosmio:openwrt-ipq:24.10-nss.diff"
             #PATCH="https://github.com/openwrt/openwrt/compare/openwrt-24.10...qosmio:openwrt-ipq:24.10-nss.diff https://github.com/openwrt/openwrt/pull/16070.diff"
-            NSSBRANCH="24.10-nss-mx4300"
+            NSSBRANCH="24.10-nss"
             ;;
     esac
 fi
