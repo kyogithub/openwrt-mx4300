4bae924de355089433f130cb7c3e6a1412df7036
Kyo <kyogithub@users.noreply.github.com>
2025-07-28 18:20:02 -0500

    Update genconfig_foss_node.sh
    Signed-off-by: Kyo <kyogithub@users.noreply.github.com>

--- script/genconfig_foss_node.sh
+++ script/genconfig_foss_node.sh
@@ -16,6 +16,7 @@ CONFIG_TARGET_qualcommax_ipq807x_DEVICE_linksys_mx4300=y
 CONFIG_TARGET_DEVICE_qualcommax_ipq807x_DEVICE_linksys_mx4300=y
 CONFIG_TARGET_DEVICE_PACKAGES_qualcommax_ipq807x_DEVICE_linksys_mx4300=\"\"
 CONFIG_PACKAGE_luci=y
+CONFIG_PACKAGE_kmod-pf-ring is not set
 " >> .config
 
 #Custom Packages for Node Router 
